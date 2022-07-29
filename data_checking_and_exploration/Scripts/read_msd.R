0fe82ffbd94c609e50d433f97df302befcc91d1c
Bourke Betz <64644141+BBetz907@users.noreply.github.com>
2022-07-29 11:41:21 -0700

    updating file name request

--- data_checking_and_exploration/Scripts/read_msd.R
+++ data_checking_and_exploration/Scripts/read_msd.R
@@ -1,13 +1,12 @@
 library(tidyverse)
 library(glamr)
 library(gophr)
-getwd()
-setwd(si_path())
+
+
 getwd()
 
 #identify file and file path
-MER_Structured_Datasets_PSNU_IM_FY20_23_20220617_v2_1 <- paste0(glamr::si_path(), "/Temp/") %>% return_latest("PSNU_IM_FY2")
-
+file <- paste0("Data/") %>% return_latest("PSNU_IM_FY2")
 
 # how to open packaged files? ---------------------------------------------
 
