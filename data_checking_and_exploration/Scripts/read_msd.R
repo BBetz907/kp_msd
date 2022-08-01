72c96f4b012f0a7b3631ef47469e706aff81453d
aditiarunmozhi@gmail.com <37117825+aditiarunmozhi@users.noreply.github.com>
2022-08-01 16:51:44 -0400

    updates to read_msd

--- data_checking_and_exploration/Scripts/read_msd.R
+++ data_checking_and_exploration/Scripts/read_msd.R
@@ -7,8 +7,6 @@ getwd()
 #identify file and file path
 file <- paste0("Data/") %>% return_latest("PSNU_IM_FY2")
 
-# how to open packaged files? ---------------------------------------------
-
 
 df <- read_msd(file, save_rds = FALSE, remove_txt = FALSE)
 # use this function to read data, see guidance
