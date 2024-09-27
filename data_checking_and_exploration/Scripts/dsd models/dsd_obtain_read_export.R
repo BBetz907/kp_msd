fe279eb6b1cbe805d8cc6693c1572ceb85b00a14
Bourke Betz <64644141+BBetz907@users.noreply.github.com>
2024-09-26 17:01:16 -0700

    urbanization class

--- data_checking_and_exploration/Scripts/dsd models/dsd_obtain_read_export.R
+++ data_checking_and_exploration/Scripts/dsd models/dsd_obtain_read_export.R
@@ -60,6 +60,7 @@
   
   files <- c(file_mli, file_cdi, file_drc)
   
+  data_source <- str_extract(file_mli, ("MER.+(?=_[A-Z][a-z]+\\.zip$)")) %>% print()
   # filenames <- map(files, ~str_c(path, .x))
   
   df <- map_dfr(files, ~read_psd(file = .x))
@@ -77,15 +78,15 @@ mwi_dic <- df |> filter(country=="Malawi",
 #confirmed that this list contains 18 DICs, no other facility types
 mwi_dic |> count(snu1, snu2, cop22_psnu, psnu, community, communityuid, facility)
 
-#generate list facilityuid with DICs
+#generate list of facilityuid with DICs
 mwi_dic_facilityuid <- mwi_dic  |> 
             group_by(facilityuid) |> summarise(.groups = "drop") |> pull()
 
-#generate list ofcommunityuid with DICs
+#generate list of communityuid with DICs
 mwi_dic_communityuid <- mwi_dic  |> 
   group_by(communityuid) |> summarise(.groups = "drop") |> pull()
 
-#generate list ofcommunityuid with DICs
+#generate list of psnuuid with DICs
 mwi_dic_psnuuid <- mwi_dic  |> 
   group_by(psnuuid) |> summarise(.groups = "drop") |> pull()
 
@@ -146,27 +147,14 @@ drc_dic <- df |> filter(
       #no filters yet to identify which health zones have DICs
       )
 
-#generate list of KP programming areas
+#generate list of  programming areas
 drc_dic |> count(snu1, snu2, cop22_psnu, psnu, community, facility)
-
-#generate list of facilityuid 
-drc_dic_facilityuid <- drc_dic |>
-  group_by(facilityuid) |> summarise(.groups = "drop") |> pull()
-
-#generate list of communityuid
-drc_dic_communityuid <- drc_dic |> 
-  group_by(communityuid) |> summarise(.groups = "drop") |> pull()
-
-#generate list of psnuuid
-drc_dic_psnuuid <- drc_dic |>   
-  group_by(psnuuid) |> summarise(.groups = "drop") |> pull()
-
 
 #generate list of KP mechanisms
 drc_kp_mech <- drc_dic |> 
   group_by(mech_code) |> summarise(.groups = "drop") |> pull()
 
-#generate list of districts with KP mechanisms
+#generate list of districts with KP mechanisms, to share with mission to ID PODIs and DIC/SDPS
 drc_kp_mech_psnu <- drc_dic |> 
   group_by(psnu) |> summarise(.groups = "drop") |> pull()
 
@@ -175,9 +163,49 @@ drc_fac <- drc_dic |> group_by(prime_partner_name, mech_code, snu1, snu1uid, com
 
 drc_comm <- drc_fac |> count(prime_partner_name, mech_code, snu1, snu1uid, community, communityuid) |> select(-n)
 
+#output list 
 write_csv(drc_comm, "dataout/drc_health_zone_list_community.csv")
 
 write_csv(drc_fac, "dataout/drc_health_zone_list_facility.csv")
+
+
+## read completed inputs from USAID DRC to ID KP DIC/SDPs and GP PODI AT HEALTH ZONE level
+# snu1 is province, community is health zone
+file_drc <- glamr::return_latest(folderpath =  path, pattern = "health_zone_list") %>% print()
+
+
+drc_org_pre <- read_xlsx(file_drc) %>% janitor::clean_names() %>% filter(nchar(mech_code) > 1) 
+drc_type <- read_xlsx(file_drc, sheet = 2) %>% janitor::clean_names() %>% glimpse()
+
+drc_org <- drc_org_pre %>% left_join(drc_type) 
+#create for filters
+drc_org_kp_comm <- drc_org  %>% 
+  filter(number_of_kp_dic_sdp > 0)
+
+#create list for detailed information
+drc_full_documentation <- drc_org %>%  filter(number_of_kp_dic_sdp > 0 | number_of_gp_podi >0) %>% 
+  group_by(mech_code, communityuid, type) %>% 
+  summarise(across(starts_with("number_of"), ~ sum(., na.rm = TRUE))) %>% ungroup()
+
+#generate list of facilityuid  - NA because it is all at community_level
+# drc_dic_facilityuid <- drc_dic |>
+#   group_by(facilityuid) |> summarise(.groups = "drop") |> pull()
+
+#generate list of communityuid
+drc_dic_communityuid <- drc_org_kp_comm |> 
+  group_by(communityuid) |> summarise(.groups = "drop") |> pull()
+
+#generate list of psnuuid
+drc_dic_psnuuid <- drc_dic |> filter(communityuid %in% drc_dic_communityuid)   %>% 
+  group_by(psnuuid) |> summarise(.groups = "drop") |> pull()
+
+
+# create combined lists to apply below
+dss_facilityuid <- map(mget(ls(pattern = "_dic_facilityuid$")), ~ .x) %>% reduce(c)
+dsd_communityuid <- map(mget(ls(pattern = "_dic_communityuid$")), ~ .x) %>% reduce(c)
+dsd_psnuuid <- map(mget(ls(pattern = "_dic_psnuuid$")), ~ .x) %>% reduce(c)
+kp_mech <- map(mget(ls(pattern = "_kp_mech$")), ~ .x) %>% reduce(c)
+kp_mech_psnu <- map(mget(ls(pattern = "_kp_mech_psnu$")), ~ .x) %>% reduce(c)
 
 ## Apply lists above to df to create analytic frame
 
@@ -187,22 +215,20 @@ af <- df |> filter(
              ) |> 
   mutate(
     #identify site org units which are DICs from lists generated above
-    site_dic = case_when(facilityuid %in% cdi_dic_facilityuid | facilityuid %in% mwi_dic_facilityuid ~ "DIC",
+    site_dic = case_when(facilityuid %in% dss_facilityuid ~ "DIC",
                           facilityuid == "~" ~ "above site",
                           .default = "not a DIC"),
     #identify community org units with DICs from lists generated above
-    community_dic = case_when(communityuid %in% cdi_dic_communityuid | communityuid %in% mwi_dic_communityuid ~ "DIC",
+    community_dic = case_when(communityuid %in% dsd_communityuid ~ "DIC",
                           .default = "no DICs"),
     #identify psnu org units with DICs from lists generated above
-    psnu_dic = case_when(psnuuid %in% cdi_dic_psnuuid | psnuuid %in% mwi_dic_psnuuid ~ "DIC",
+    psnu_dic = case_when(psnuuid %in% dsd_psnuuid ~ "DIC",
                           .default = "no DICs"),
     #Identify KP mechanisms from lists above
-    mech_type = case_when(mech_code %in% cdi_kp_mech ~ "KP",
-                               mech_code %in% mwi_kp_mech ~ "KP",
+    mech_type = case_when(mech_code %in% kp_mech ~ "KP",
                                .default = "other"),
     #Identify psnus with KP mechanisms from lists above
-    psnu_mech_type = case_when(psnu %in% cdi_kp_mech_psnu ~ "KP mech in district",
-                          psnu %in% mwi_kp_mech_psnu ~ "KP mech in district",
+    psnu_mech_type = case_when(psnu %in% kp_mech_psnu ~ "KP mech in district",
                           .default = "other"),
     
          ) |> 
@@ -224,10 +250,37 @@ af <- df |> filter(
                               # psnu %in% cdi_kp_mech_psnu | 
                                 # psnu %in%  mwi_kp_mech_psnu, 
                               "no DIC; KP mechanism", 
-                              "no DIC; no KP mechanism")
-         )) |> 
+                              "no DIC; no KP mechanism"),),
+           data_source = data_source,
+         ) |> 
+  #establish type as urban or rural or null. DRC has rural and urban input. Others only urban.
+  left_join(drc_type, by = c("snu1" = "province", "community" = "health_zone")) %>% 
+  mutate(type = if_else(!is.na(type),
+                        type,
+                        case_when(country == "Cote d'Ivoire" & (str_detect(snu1, "Abidjan") | str_detect(snu2, "Bouake") ) ~ "Urban",
+                                  country == "Malawi" & (str_detect(psnu, "Lilongwe|Blantyre") ) ~ "Urban"
+                        ))) %>%
   glimpse()
 
+#create max periods info
+max_fyy <- af %>% filter(period_type == "cumulative") %>% 
+  count(period) %>% 
+  mutate(max_fy = max(str_extract(period, "(?<=FY)[0-9]{2}"))) %>% 
+  count(max_fy) %>% pull(max_fy)
+
+max_dates <- af %>% filter(period_type=="results",
+                         str_detect(period, max_fyy)) %>% 
+  count(period) %>% 
+  mutate(max_q = max(str_extract(period, "[1-4]$")),
+         max_fy = str_extract(period, "FY[0-9]{2}"),
+         max_fyq = str_c(max_fy, max_q, sep = "Q"),
+         max_qq = str_c("Q", max_q)) %>% 
+  select(contains("max")) %>% 
+  group_by_all() %>% summarise() %>%  
+  mutate(join=1) %>% 
+  print() 
+
+  
 # # assess orgunit structure by country ----
 af |> filter(country == "Malawi") |>  group_by(country, snu1, snu2, psnu, cop22_psnu, community) |> summarise()
 af |> filter(country != "Malawi", community != "Data reported above Community Level" ) |>
@@ -247,6 +300,9 @@ af |> filter(country != "Malawi", community != "Data reported above Community Le
 #   group_by(country, site_dic) |> 
 #   summarise(n = n_distinct(sny, facility))
 
+#add max info
+wf <- af %>% mutate(join=1) %>% inner_join(max_dates)
+
 # output -------
-write_csv(af, "Dataout/dsd_cdi_mwi.csv")
+write_csv(wf, "Dataout/dsd_cdi_mwi.csv")
 
